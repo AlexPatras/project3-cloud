50586e9cfaefbecc56b438c70f3f754c92e6fff4
AlexandruPatras <a.patras@student.fontys.nl>
2024-01-10 02:21:58 +0100

    Update db.datasource.ts

--- backend-copy/stalker/src/datasources/db.datasource.ts
+++ backend-copy/stalker/src/datasources/db.datasource.ts
@@ -4,7 +4,8 @@ import {juggler} from '@loopback/repository';
 const config = {
   name: 'db',
   connector: 'postgresql',
-  url: 'postgres://postgres:mypw@localhost:5432',
+   // url :postgres://postgres:mypw@localhost:5432/database?ssl=true
+  url: 'postgres://project3_cloud_postgres_user:lAPwcnhNPVeV6SeYttaas8YwlJ0L4pCN@dpg-cmeuptacn0vc73bu5at0-a.frankfurt-postgres.render.com:5432/project3_cloud_postgres?ssl=true'
 };
 
 // Observe application's life cycle to disconnect the datasource when
